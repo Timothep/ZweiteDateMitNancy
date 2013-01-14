8d4cde885e3a031269859c0fb577bd16fc13261d
Tim Bourguignon <Timothepdotnet@gmail.com>
2013-01-14 23:54:11 +0100

    First draft of the article

---
+++ ZweiteDateMitNancy/CityInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace ZweiteDateMitNancy
+{
+    public class CityInfo
+    {
+        public string Id { get; set; }
+        public string city { get; set; }
+        public int pop { get; set; }
+        public string state { get; set; }
+        public double[] loc { get; set; }
+    }
+}
---
+++ ZweiteDateMitNancy/Module.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace ZweiteDateMitNancy
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Threading;
+    using Nancy;
+    using Simple.Data;
+    using Simple.Data.MongoDB;
+    using System.Linq;
+
+    public class Module : NancyModule
+    {
+        private const string ConnectionString = @"mongodb://localhost:27017/zipDB";
+        private readonly dynamic db = Database.Opener.OpenMongo(ConnectionString);
+
+        public Module()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+
+            Get["/zip/{id}"] = parameters =>
+                {
+                    CityInfo cityInfo = db.zips.FindById(parameters.id.ToString());
+                    return Negotiate.WithModel(cityInfo)
+                                    .WithView("SingleView");
+                };
+
+            Get["/city/{name}"] = parameters =>
+                {
+                    IList<CityInfo> cityInfoList = db.zips.FindAllBy(city: parameters.name.ToString()).ToList();
+                    return Negotiate.WithModel(cityInfoList)
+                                    .WithView("MultipleView");
+                };
+        }
+    }
+}
